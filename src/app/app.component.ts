faec531df054e6ba8a27dc6470747e693c220fc3
milossilhar <migmig095@gmail.com>
2025-03-16 21:29:52 +0100

    chg: init angular with primeng and tailwind

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [ ButtonModule ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+
+import { routes } from './app.routes';
+import HorizontAppTheme from './app.theme';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+          preset: HorizontAppTheme,
+          options: {
+            darkModeSelector: false
+          }
+      }
+  })
+  ]
+};
