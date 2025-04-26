23a62b4e2c05a1ec4a4275d994c14a2da780ac61
milossilhar <migmig095@gmail.com>
2025-04-26 09:04:43 +0200

    chg: new components for unavailable and notfound

---
+++ src/app/standalone/unavailable/unavailable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unavailable',
+  imports: [],
+  templateUrl: './unavailable.component.html',
+  styles: ``
+})
+export class UnavailableComponent {
+
+}
