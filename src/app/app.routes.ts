20972b7efcc2da13fd8b883a19a0f7b3cc21e5bb
milossilhar <migmig095@gmail.com>
2025-06-04 14:37:20 +0200

    fix: deleted imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,6 @@ import { RegistrationListComponent } from './registration/registration-list/regi
 import { InitGuard } from './shared/guards/init.guard';
 import { UnavailableComponent } from './standalone/unavailable/unavailable.component';
 import { NotFoundComponent } from './standalone/not-found/not-found.component';
-import { RegistrationResultQueueComponent } from './registration/registration-result-queue/registration-result-queue.component';
-import { RegistrationResultSuccessComponent } from './registration/registration-result-success/registration-result-success.component';
 import { RegistrationConfirmComponent } from './registration/registration-confirm/registration-confirm.component';
 import { EventDetailComponent } from './event/event-detail/event-detail.component';
 import { AuthGuard } from './shared/guards/auth.guard';
