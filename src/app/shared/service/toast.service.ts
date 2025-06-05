857febb5d46a19b8576ec55dc33b88f5d6d28c9b
milossilhar <migmig095@gmail.com>
2025-06-05 18:37:59 +0200

    new: services for dialogs and toast messages

---
+++ src/app/shared/service/toast.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { NgToastSeverity } from '../enum/prime-ng-severities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  public success(message: string, header: string) {
+    this.msg('success', message, header);
+  }
+
+  public warn(message: string, header: string) {
+    this.msg('warn', message, header);
+  }
+
+  public error(message: string, header: string) {
+    this.msg('error', message, header);
+  }
+
+  private msg(severity: NgToastSeverity, message: string, header: string, ttl: number = 4000) {
+    this.messageService.add({
+      severity: severity,
+      summary: header,
+      detail: message,
+      life: ttl
+    });
+  }
+}
