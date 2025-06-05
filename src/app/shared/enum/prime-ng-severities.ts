27b4a0c9078002d7026330b78a776145af058ec2
milossilhar <migmig095@gmail.com>
2025-06-05 18:37:33 +0200

    new: typpings

---
+++ src/app/shared/enum/prime-ng-severities.ts
@@ -0,0 +1,9 @@
+/**
+ * Severity used in PrimeNG TAG
+ */
+export type NgTagSeverity = undefined | 'secondary' | 'success' | 'info' | 'warn' | 'danger' | 'contrast';
+
+/**
+ * Severity used in PrimeNG TOAST
+ */
+export type NgToastSeverity = undefined | 'success' | 'info' | 'warn' | 'error' | 'secondary' | 'contrast';
