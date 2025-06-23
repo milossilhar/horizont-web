0e27ef985ac50a237aa6b3033937c1e76327dbd8
milossilhar <migmig095@gmail.com>
2025-06-23 12:53:27 +0200

    new: sort parameter interceptor

--- src/app/http/interceptors.ts
+++ src/app/http/interceptors.ts
@@ -1,7 +1,9 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Provider } from '@angular/core';
 import { ContentTypeInterceptor } from './content-type.interceptor';
+import { SortParameterInterceptor } from './sort-parameter.interceptor';
 
 export const HTTP_INTERCEPTOR_PROVIDERS: Provider[] = [
-  { provide: HTTP_INTERCEPTORS, useClass: ContentTypeInterceptor, multi: true }
+  { provide: HTTP_INTERCEPTORS, useClass: ContentTypeInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: SortParameterInterceptor, multi: true }
 ];
---
+++ src/app/http/sort-parameter.interceptor.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { forEach } from 'lodash';
+import { Observable } from 'rxjs';
+
+export class SortParameterInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const sortParams = req.params.getAll('sort');
+
+    if (!sortParams || sortParams.length === 0) return next.handle(req);
+
+    if (sortParams.length === 1) {
+      const parsedSortParams = JSON.parse(sortParams[0]);
+      if (Array.isArray(parsedSortParams)) {
+        let params = req.params.delete('sort');
+        
+        forEach(parsedSortParams, value => {
+          params = params.append('sort', value);
+        });
+
+        return next.handle(req.clone({ params }));
+      }
+    }
+
+    return next.handle(req);
+
+  }
+
+}
